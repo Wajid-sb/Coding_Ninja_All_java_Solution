a4b3679a7736f3436dedadd8ed2f3f47b15d283f
Wajid-sb <84681950+Wajid-sb@users.noreply.github.com>
2021-11-02 03:35:38 +0530

    Create sum_Of_Digits_recursion.java

---
+++ sum_Of_Digits_recursion.java
@@ -0,0 +1,10 @@
+public class solution {
+
+	public static int sumOfDigits(int input){
+		// Write your code here
+        if(input<=0){
+            return input;
+        }
+        return input%10+sumOfDigits(input/10);
+	}
+}
